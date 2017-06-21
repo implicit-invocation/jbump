694a026db0ed8961648a093a0066ae74c15c775e
Duong Dat <tao@cu8-2.local>
2017-06-22 01:03:03 +0700

    adjust delta

--- src/com/dongbat/jbump/Aux.java
+++ src/com/dongbat/jbump/Aux.java
@@ -21,7 +21,7 @@ package com.dongbat.jbump;
  */
 public class Aux {
 
-  public static final float DELTA = 1e-10f;
+  public static final float DELTA = 1e-5f;
 
   public static int sign(float x) {
     if (x > 0) {
